767dee1a3fd96d267d4524561a98bf6d02d2068f
moth <moth>
2022-05-13 14:33:29 -0500

    made mod to parse json into struct [trans rights]

---
+++ src/addresses.rs
@@ -0,0 +1,26 @@
+use json::*;
+
+pub struct Index {
+    title: String,
+    m3u8: String,
+    tsa: Vec<String>,
+}
+
+pub fn new(inp: &str) -> Result<Index> {
+    let ind = json::parse(inp).unwrap();
+
+    let mut tsa = Vec::new();
+    let tsain = ind["tsa"].members().as_slice().to_vec();
+
+    for i in 0..tsain.len() {
+        tsa.push(tsain[i].as_str().unwrap().to_owned());
+    }
+
+    let out = Index { 
+        title: ind["title"].as_str().unwrap().to_owned(),
+        m3u8: ind["m3u8"].as_str().unwrap().to_owned(),
+        tsa: tsa,
+    };
+
+    Ok(out)
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use ipfs_api::*;
+
+pub mod addresses;
+
+#[tokio::main]
+pub async fn serve(hash: &str) {
+
+}

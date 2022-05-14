4acdae69164ec32972ca6b9e17519dcce8b67be3
moth <moth>
2022-05-13 19:58:47 -0500

    created basic function to query ipfs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
 use ipfs_api::*;
+use futures_util::stream::*;
 
 pub mod addresses;
 
 #[tokio::main]
-pub async fn serve(hash: &str) {
+pub async fn serve(hash: &str) -> Result<String, String> {
+    let client = ipfs_api::IpfsClient::default();
 
+    let gotten = String::from_utf8(client.cat(hash).map_ok(|chunk| chunk.to_vec()).try_concat().await.unwrap()).unwrap();
+
+    Ok(gotten)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::serve;
+
+    #[test]
+    fn ipfscontest() {
+        println!("{}", serve("QmRf22bZar3WKmojipms22PkXH1MZGmvsqzQtuSvQE3uhm").unwrap());
+    }
 }

7983ad12dd297ee92028ed476b20d38e2968f89e
moth <moth>
2022-05-13 20:16:09 -0500

    changed function name

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use futures_util::stream::*;
 pub mod addresses;
 
 #[tokio::main]
-pub async fn serve(hash: &str) -> Result<Vec<u8>, Error> {
+pub async fn get(hash: &str) -> Result<Vec<u8>, Error> {
     let client = ipfs_api::IpfsClient::default();
 
     let gotten = client.cat(hash).map_ok(|chunk| chunk.to_vec()).try_concat().await.unwrap();
@@ -14,10 +14,10 @@ pub async fn serve(hash: &str) -> Result<Vec<u8>, Error> {
 
 #[cfg(test)]
 mod tests {
-    use crate::serve;
+    use crate::get;
 
     #[test]
     fn ipfscontest() {
-        println!("{:?}", String::from_utf8(serve("QmRf22bZar3WKmojipms22PkXH1MZGmvsqzQtuSvQE3uhm").unwrap()).unwrap());
+        println!("{:?}", String::from_utf8(get("QmRf22bZar3WKmojipms22PkXH1MZGmvsqzQtuSvQE3uhm").unwrap()).unwrap());
     }
 }
